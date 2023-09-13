9366c1e46847349b72b7c9b412fdab282cb231f6
carolCase <carolinazona7@gmail.com>
2023-09-13 15:24:35 +0200

    arrays and logics

--- colorLogic.js
+++ colorLogic.js
@@ -1,1 +1,32 @@
-document.getElementById("my-Color-Id").style.color ="red"
+
+function changeColor() {
+    document.getElementById("my-Color-Id").style.color="red"
+    
+}
+
+function variables(){
+
+    var name = ""
+    var age=0
+
+     name= "Carolina "
+     age= 41
+    alert(name+age)
+    console.log(name+age);
+}
+
+function arrays() {
+    var score = [100, 150 ,250 ,1000]
+    alert(score)
+    
+}
+//index
+//element
+function userLogin() {
+    var result = prompt("please enter your user name")
+    alert(result)
+    
+    
+}
+
+
